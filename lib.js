4a4974f219870687b695eaa258165929660940f7
Andrew Killillay <68135849+ChipSlayer1@users.noreply.github.com>
2022-02-02 16:01:40 -0600

    Create lib.js

---
+++ lib.js
@@ -0,0 +1,42 @@
+// initial global variables
+var canvas;
+var ctx;
+var width;
+var height;
+var mouseX;
+var mouseY;
+
+// initiate canvas stuff
+function __init__(w, h, node) {
+    canvas = document.createElement("canvas");
+	ctx = canvas.getContext("2d");
+	width = canvas.width = w;
+	height = canvas.height = h;
+	node.body.appendChild(canvas);
+}
+
+// button class
+function Button(label, x, y, rad, width, height, draw, change, func, font, col1, col2) {
+	this.l = label;
+	this.x = x;
+	this.y = y;
+	this.r = r;
+	this.w = w;
+	this.h = h;
+	this.d = d;
+	this.c = c;
+	this.f = func;
+	this.font = font;
+	this.c1 = col1;
+	this.c2 = col2;
+	
+	// mouseOver function
+	
+}
+
+// detect mouse clicks
+function listen_for_click(func) {
+	window.addEventListener("click", function() {
+		func();
+	});
+}
